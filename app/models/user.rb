1d9e09f5bac8e52043172c4aa2e78f0a5da8f1ed
gotti45 <yuto.yogotti35@icloud.com>
2020-11-23 18:58:09 +0900

    ユーザー登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 6..128
+  
+  VALID_PASSWORD_REGEX = /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  VALID_KANA_REGEX = /\A[ァ-ン]+\z/
+
+  validates :email, presence: true
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :family_katakana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :first_katakana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :birthday, presence: true
+
 end
