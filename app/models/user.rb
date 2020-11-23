fed5d1f007fc03e7f317539d46a17967e38310a0
gotti45 <yuto.yogotti35@icloud.com>
2020-11-23 19:15:26 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 6..128
   
-  VALID_PASSWORD_REGEX = /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i
+  VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
   VALID_KANA_REGEX = /\A[ァ-ン]+\z/
 
