bd8cc6c1701b6144c5ebe14127d679f64c2a12c7
gotti45 <yuto.yogotti35@icloud.com>
2020-11-26 18:20:24 +0900

    詳細画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! , except: [:index]
+  before_action :move_to_index, only: [:edit, :update]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,7 +20,19 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
   private
+
+  def move_to_index 
+    @item = Prototype.find(params[:id])
+    unless current_user.id ==  @item.user.id
+        redirect_to root_path
+    end
+  end
 
   def item_params
     params.require(:item).permit(:name, :text, :cost_id, :shipping_day_id, :price, :category_id, :shipping_address_id, :status_id, :image).merge(user_id: current_user.id)
